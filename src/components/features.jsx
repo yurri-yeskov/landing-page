06c0467049668f1080173c1a8d7e640fe9e69f34
Yurri Yeskov <83100719+yurri-yeskov@users.noreply.github.com>
2021-09-24 07:49:54 -0700

    change some layout

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -2,7 +2,7 @@ export const Features = (props) => {
   return (
     <div id='features' className='text-center'>
       <div className='container'>
-        <div className='col-md-10 col-md-offset-1 section-title'>
+        <div className='col-md-10 col-md-offset-1 section-title' style={{paddingTop: "90px"}}>
           <h2>Features</h2>
         </div>
         <div className='row'>
